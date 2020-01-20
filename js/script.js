c9bcaff32bfcda68cc9e8943bd43f1cec4b786bb
bamboleylo <tsybulko.artem@gmail.com>
2020-01-20 11:38:13 +0300

    Update slider

--- js/script.js
+++ js/script.js
@@ -1,6 +1,5 @@
 window.onload = function () {
     //Активация блоков по клику
-
     function activeBlock (nameOfClass, nameOfClassChange) {
         let arr = document.getElementsByClassName(nameOfClass);
         for (let item of arr) {
@@ -20,13 +19,18 @@ window.onload = function () {
     function slider (idOfPrevButton, idOfNextButton, idOfSlides, classOfActiveSlide, idOfRound, idOfActiveRound) {
         const prevButton = document.getElementById(idOfPrevButton);
         const nextButton = document.getElementById(idOfNextButton);
+        let slideImages = ["Rectangle.jpg", "zaliv.jpg", "norilsk.jpg", "magadan.jpg"];
         let activeTextId = 1;
         nextButton.addEventListener("click", () => {
             if (activeTextId === 4) {
                 activeTextId = 1;
             } else {
                 activeTextId++;
-            }
+            };
+            if (idOfNextButton == "next") {
+                document.getElementById("first_screen").style.backgroundImage = `url(./img/${slideImages[activeTextId - 1]})`; 
+            };
+
             document.getElementById(idOfSlides + String(activeTextId == 1 ? 4 : activeTextId - 1)).classList.remove(classOfActiveSlide);
             document.getElementById(idOfSlides + String(activeTextId)).classList.add(classOfActiveSlide);    
             
@@ -38,7 +42,10 @@ window.onload = function () {
                 activeTextId = 4;
             } else {
                 activeTextId--;
-            }
+            };
+            if (idOfPrevButton == "prev") {
+                document.getElementById("first_screen").style.backgroundImage = `url(./img/${slideImages[activeTextId - 1]})`; 
+            };
             document.getElementById(idOfSlides + String(activeTextId == 4 ? 1 : activeTextId + 1)).classList.remove(classOfActiveSlide);
             document.getElementById(idOfSlides + String(activeTextId)).classList.add(classOfActiveSlide);
 
